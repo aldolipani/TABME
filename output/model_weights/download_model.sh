92dc0e01f9256d3f1d4b225c8afc796858bb907f
GiftMungmeeprued <89731089+GiftMungmeeprued@users.noreply.github.com>
2022-09-21 10:02:34 +0100

    Update download_model.sh

--- output/model_weights/download_model.sh
+++ output/model_weights/download_model.sh
@@ -3,16 +3,16 @@
 cd output/model_weights
 
 echo download full model
-wget https://www.dropbox.com/s/uc10gl58h6t6c62/full_model.zip?dl=1
+wget https://www.dropbox.com/s/uc10gl58h6t6c62/full_model.zip
 unzip full_model.zip
 rm full_model.zip
 
 echo download model with resnet ablation
-wget https://www.dropbox.com/s/iwzp94zsj0g8syq/minus_resnet.zip?dl=1
+wget https://www.dropbox.com/s/iwzp94zsj0g8syq/minus_resnet.zip
 unzip minus_resnet.zip
 rm minus_resnet.zip
 
 echo download model with layoutlm ablation
-wget https://www.dropbox.com/s/6f2akulr4labhtz/minus_layoutlm.zip?dl=1
+wget https://www.dropbox.com/s/6f2akulr4labhtz/minus_layoutlm.zip
 unzip minus_layoutlm.zip
 rm minus_layoutlm.zip
