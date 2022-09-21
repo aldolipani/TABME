0e6765cfa3fdf41b0a1646f7e9ccc2e6d42d664a
GiftMungmeeprued <89731089+GiftMungmeeprued@users.noreply.github.com>
2022-09-21 10:05:16 +0100

    Update download_dataset.sh

--- data/download_dataset.sh
+++ data/download_dataset.sh
@@ -3,16 +3,16 @@
 cd data
 
 echo download train.zip
-wget https://www.dropbox.com/s/ohyhm2v3bhjf9l5/train.zip?dl=1
+wget https://www.dropbox.com/s/ohyhm2v3bhjf9l5/train.zip
 unzip train.zip
 rm train.zip
 
 echo download val.zip
-wget https://www.dropbox.com/s/bw4uhwlnkamq4rd/val.zip?dl=1
+wget https://www.dropbox.com/s/bw4uhwlnkamq4rd/val.zip
 unzip val.zip
 rm val.zip
 
 echo download test.zip
-wget https://www.dropbox.com/s/bw4uhwlnkamq4rd/val.zip?dl=1
+wget https://www.dropbox.com/s/bw4uhwlnkamq4rd/val.zip
 unzip test.zip
 rm test.zip
